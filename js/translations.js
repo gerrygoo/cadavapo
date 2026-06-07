1de96523b1a407b7df747406e56a42e6c5d652b7
ggo <gerrygoable@outlook.com>
2026-06-07 11:51:43 -0600

    add ES/EN translations

---
+++ js/translations.js
@@ -0,0 +1,32 @@
+const T = {
+  es: {
+    roles: [
+      "dirección creativa",
+      "artista audiovisual",
+      "production designer",
+      "art historian",
+      "set dresser"
+    ],
+    nav: {
+      about:    "sobre mí",
+      projects: "proyectos",
+      contact:  "contacto"
+    },
+    langBtn: "idiomas"
+  },
+  en: {
+    roles: [
+      "creative director",
+      "audiovisual artist",
+      "production designer",
+      "art historian",
+      "set dresser"
+    ],
+    nav: {
+      about:    "about me",
+      projects: "projects",
+      contact:  "contact"
+    },
+    langBtn: "language"
+  }
+};
